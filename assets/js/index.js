adf4646593ce9f1b524a52f7611a1cd029a85402
Cesar Augusto <cesaros14@gmail.com>
2016-07-23 08:46:57 -0500

    - Add styles to Hello

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,28 @@
+// Require SCSS to allow Webpack live render
 require('../css/style.scss');
 
+// Import React and React-dom
 import React from 'react'
 import { render } from 'react-dom'
+// Import elements of React Router
+import { Router, Route, browserHistory, IndexRoute, Link } from 'react-router'
 
+// Define app container
+var destination = document.querySelector("#app");
 
-render(<h1>Hello</h1>, document.getElementById('app'))
+// Define the main App component
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+});
+
+// Start Here
+render(
+  <App />
+  , destination
+)
