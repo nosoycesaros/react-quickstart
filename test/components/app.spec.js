655f1233311e9bd9cdafcef32b2c435d78b2090b
Andres Osorio <androideosorio@me.com>
2016-07-23 16:33:23 -0500

    feat(tests): add npm tasks for tests watching (so tests are reloaded and re-run when modified)

--- test/components/app.spec.js
+++ test/components/app.spec.js
@@ -4,17 +4,28 @@ import { renderComponent } from '../helpers';
 // import react component
 import App from '../../assets/js/App';
 
+/**
+ * Application Component specs
+ */
 describe('App Component', () => {
   let component, props;
   const initialProps = {};
 
+  // render the component and get it, along with the props
+  // it now has after rendering
   before(() => {
     ({ props, component } = renderComponent(App, initialProps));
   });
 
+  // the app should greet the user
   it('should say hello', () => {
     // get the component's children
     const paragraph = component.props.children;
     expect(paragraph.props.children).to.equal('Hello');
+  });
+
+  // make sure the component has the appropiate classes
+  it('should have a container', () => {
+    expect(component.props.className).to.equal('container');
   });
 })
