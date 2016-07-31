508862c51db4b9557d29301e22c31de252cfca27
Andres Osorio <androideosorio@me.com>
2016-07-31 15:06:26 -0500

    feat(prod-build): add config files for different environments and resolve the webpack config to the appropiate file

---
+++ config/webpack.development.js
@@ -0,0 +1,29 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './assets/js/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('assets/css/style.css', {
+      allChunks: true
+    })
+  ]
+}
